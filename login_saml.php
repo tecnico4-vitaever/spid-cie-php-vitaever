b005af02f2c0c5ee57d43801b69eb7bb49d79685
Carlo <carlo.tamburrelli@vitaever.com>
2026-03-05 12:08:32 +0100

    Fix production env

--- login_saml.php
+++ login_saml.php
@@ -34,8 +34,10 @@ const DEBUG = false;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+$production = isset($_ENV) ? $_ENV['IS_PRODUCTION']: false;
+
 $proxy_config = file_exists(PROXY_CONFIG_FILE)? json_decode(file_get_contents(PROXY_CONFIG_FILE), true) : array();
-$production = $proxy_config['production'];
+//$production = $proxy_config['production'];
 
 $clients        = $proxy_config['clients'];
 
@@ -70,8 +72,10 @@ if(in_array($client_id, array_keys($clients)) && (isset($_GET['redirect_uri']) |
         $service = $clients[$client_id]['service'];
     }
 
-    $isCIE = ($idp == "CIE" || $idp == "CIE TEST");
-    $service = $isCIE ? "cie" : $service;
+    if ($idp=="CIE" || $idp=="CIE TEST") {
+        $service = "cie";
+        $idp = $production ? "CIE" : "CIE TEST";
+    }
 
     $spidsdk = new SPID_PHP($production, $service);
 
--- login_test.php
+++ login_test.php
@@ -14,8 +14,10 @@
 
     $service = "spid";
     $idp = $_REQUEST['idp'];
-    if($idp=="CIE" || $idp=="CIE TEST") $service = "cie";
-
+    if ($idp=="CIE" || $idp=="CIE TEST") {
+        $service = "cie";
+        $idp = $production == true ? "CIE" : "CIE TEST";
+    }
 
     $spidsdk = new SPID_PHP($production, $service);
 
